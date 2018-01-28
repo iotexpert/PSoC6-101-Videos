37f07e7d0c07cc7e7cdeb09396e8d6a59c962927
Alan Hawse <arh@Alans-MacBook-Pro.local>
2018-01-28 17:37:13 -0500

    Final Updates to SimpleBLE Central

--- SimpleBLECentral.cydsn/main_cm4.c
+++ SimpleBLECentral.cydsn/main_cm4.c
@@ -4,42 +4,67 @@
 #include "task.h"
 #include <stdio.h>
 
-#define SIMPLEBLEPERIPHERAL_NAME "P6LED"
-#define SIMPLEBLEPERIPHERAL_LEN 28
-const uint8_t ledService[] = {0x27, 0x74, 0x70,0xF7,0x6D, 0x4B, 0x49, 0x82, 0x01, 0x4a, 0x9e, 0xcd, 0x56, 0x04, 0x55, 0x50};
+// These 
+#define SBP_NAME "P6LED"
+#define SBP_NAME_OFFET 5
+#define SBP_ATT_LEN 28
+#define SBP_SERVICE_OFFSET 12
+
+
+#define BRIGHT_INCREMENT 10
+#define BRIGHT_MAX 100
+
+void writeLed(uint8_t brightness)
+{
+    
+    if(Cy_BLE_GetConnectionState( cy_ble_connHandle[0]) != CY_BLE_CONN_STATE_CLIENT_DISCOVERED)
+    {
+        printf("Not connected\n");
+        return;
+    }
+
+    printf("Brightness = %d\r\n",brightness);
+    
+    cy_stc_ble_gattc_write_req_t myVal;
+    myVal.handleValPair.attrHandle = cy_ble_customCServ [CY_BLE_CUSTOMC_LED_SERVICE_INDEX].customServChar[CY_BLE_CUSTOMC_LED_GREEN_CHAR_INDEX].customServCharHandle[0];
+    myVal.handleValPair.value.val = &brightness;
+    myVal.handleValPair.value.actualLen = 1;
+    myVal.handleValPair.value.len = 1;
+    myVal.connHandle = cy_ble_connHandle[0];
+    
+    if(Cy_BLE_GATTC_WriteCharacteristicValue( &myVal ) != CY_BLE_SUCCESS)
+        printf("gatc write error\n\r");
+}
 
 void customEventHandler(uint32_t event, void *eventParameter)
-{
-    cy_stc_ble_gatts_write_cmd_req_param_t   *writeReqParameter;
+{   
     cy_stc_ble_gapc_adv_report_param_t  *scanProgressParam;
     
-    /* Take an action based on the current event */
     switch (event)
     {
         /* This event is received when the BLE stack is Started */
         case CY_BLE_EVT_STACK_ON:
         case CY_BLE_EVT_GAP_DEVICE_DISCONNECTED:
-            printf("Starting Scan\n\r");
-          
+            printf("Starting Scan\r\n");
             Cy_BLE_GAPC_StartScan(CY_BLE_SCANNING_FAST,0);  
         
         break;
         case CY_BLE_EVT_GAPC_SCAN_PROGRESS_RESULT:
-            printf("Found Device ");
+            // Print Out Information about the Device that was found
+            printf("Device ");
             scanProgressParam = (cy_stc_ble_gapc_adv_report_param_t  *)eventParameter;
             printf("BD Addr = ");
             for(unsigned int i=0;i<CY_BLE_BD_ADDR_SIZE;i++)
-            {
                 printf("%2X",scanProgressParam->peerBdAddr[i]);
-            }
             
             printf(" Length = %d\r\n",scanProgressParam->dataLen);
-            
-            if(scanProgressParam->dataLen == SIMPLEBLEPERIPHERAL_LEN &&
-                strncmp(SIMPLEBLEPERIPHERAL_NAME,&scanProgressParam->data[5],strlen(SIMPLEBLEPERIPHERAL_NAME)) == 0 
-               &&   memcmp(&ledService[0],&scanProgressParam->data[12],16) == 0)
+
+            // If it is the P6LED then make a connection and stop scanning
+            if(scanProgressParam->dataLen == SBP_ATT_LEN 
+               && strncmp(SBP_NAME,(const char *)&scanProgressParam->data[SBP_NAME_OFFET],strlen(SBP_NAME)) == 0 
+               && memcmp(cy_ble_customCServ [CY_BLE_CUSTOMC_LED_SERVICE_INDEX].uuid,&scanProgressParam->data[SBP_SERVICE_OFFSET],16) == 0)
             {
-                printf("Found P6LED\r\n");
+                printf("Found %s\r\n",SBP_NAME);
                 // make a connection
                 cy_stc_ble_bd_addr_t connectAddr;
                 memcpy(&connectAddr.bdAddr[0], &scanProgressParam->peerBdAddr[0] , CY_BLE_BD_ADDR_SIZE);
@@ -50,17 +75,24 @@ void customEventHandler(uint32_t event, void *eventParameter)
         break;
 
         case CY_BLE_EVT_GATT_CONNECT_IND:
-            printf("Made a connection\r\n");
-            
-            
+            printf("Made a connection starting service discovery\r\n");
+            Cy_BLE_GATTC_StartDiscovery(cy_ble_connHandle[0]);
         break;
         
-        
-        case CY_BLE_EVT_GATTS_WRITE_REQ:
+        case CY_BLE_EVT_GATTC_DISCOVERY_COMPLETE:
+            printf("Discovery Complete\r\n" );
         break;
-        
+            
+        case CY_BLE_EVT_GATTC_ERROR_RSP:
+            printf("GattC Error Response\r\n");
+        break;
+            
+        case CY_BLE_EVT_GATTC_WRITE_RSP:
+            printf("GATC Write Succeeded\r\n");
+        break;
+         
         default:
-            break;
+        break;
     }
 }
 
@@ -70,11 +102,26 @@ void bleTask(void *arg)
     
     Cy_BLE_Start(customEventHandler);
     
-    PWM_DIM_Start();
-    
+    char c;
+    uint8_t brightness = 0;
     for(;;)
     {
-         Cy_BLE_ProcessEvents();
+        Cy_BLE_ProcessEvents();
+        if(Cy_SCB_UART_GetNumInRxFifo(UART_1_HW))
+        {
+            c = getchar();
+            switch(c)
+            {
+                case '+': // Increase the brightness and then send it via BLE
+                    brightness = brightness + BRIGHT_INCREMENT > BRIGHT_MAX? BRIGHT_MAX : brightness + BRIGHT_INCREMENT;
+                    writeLed(brightness);
+                break;
+                case '-': // Decrease the brightness and then send it via BLE
+                    brightness = (brightness > BRIGHT_INCREMENT ? brightness - BRIGHT_INCREMENT : 0); 
+                    writeLed(brightness);
+                break;    
+            }
+        }
     }
 }
 
@@ -82,11 +129,10 @@ int main(void)
 {
     __enable_irq(); /* Enable global interrupts. */
     UART_1_Start();
+    setvbuf(stdin,0,_IONBF,0);
+    
     printf("Started\n");
-
     xTaskCreate(bleTask,"bleTask",8*1024,0,1,0);
     vTaskStartScheduler();
  
 }
-
-/* [] END OF FILE */
